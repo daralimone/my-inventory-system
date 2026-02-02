c5364eaa3bc89ef4e0be1f1917ef022eb0cd69fb
ឈ្មោះរបស់អ្នក <email_@example.com>
2026-02-03 06:10:55 +0700

    update

--- webapp.py
+++ webapp.py
@@ -4,9 +4,7 @@ import pandas as pd
 import time
 import io
 from extra_streamlit_components import CookieManager
-from st_gsheets_connection import GSheetsConnection # type: ignore
-
-# --- ១. ការកំណត់ទំព័រ ---
+from st_gsheets_connection import GSheetsConnection
 st.set_page_config(page_title="ប្រព័ន្ធគ្រប់គ្រងអាជីវកម្ម", layout="wide")
 cookie_manager = CookieManager()
 
