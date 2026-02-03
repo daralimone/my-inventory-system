9fe70e3ce1f7133717fa1b5dad1d25aa36647d2f
ឈ្មោះរបស់អ្នក <email_@example.com>
2026-02-03 07:31:44 +0700

    update

--- webapp.py
+++ webapp.py
@@ -58,8 +58,8 @@ def login():
 if login():
     with st.sidebar:
         # បង្ហាញ Logo និងឈ្មោះហាង One
-        st.image("https://scontent-ams2-1.xx.fbcdn.net/v/t39.30808-6/616852928_1482523783878367_9007683925897413730_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=hkJ9o0otzd0Q7kNvwFh8E_0&_nc_oc=AdlhXgG1fUL2U928bjoqnc1mbK6q6kj5VZ9GV0n4nlYUU7ST2zkvVczih-lZsiU3fe_Lim-Siw57v-8Bud-iwTfY&_nc_zt=23&_nc_ht=scontent-ams2-1.xx&_nc_gid=uCN-lQ4gIIcV3sExa7R3-Q&oh=00_AfvHGs6tCVQXUFa1ZrztUegPWshZH6fRm6F5GQ0OibcPsQ&oe=6986F8A7", width=100)
-        st.title("One Store")
+        st.image("https://www.facebook.com/photo/?fbid=122173235942433880&set=a.122094568268433880", width=100)
+        st.title("មួយ")
         
         st.header("📝 គ្រប់គ្រងទិន្នន័យ")
         with st.form("add_product", clear_on_submit=True):
