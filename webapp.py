a4ea98595f48e76fa5c8da8b53fbd022f1752adb
ឈ្មោះរបស់អ្នក <email_@example.com>
2026-02-03 20:25:46 +0700

    Fix: Removed typo 'git' from FPDF import

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ import io
 import requests
 from sqlalchemy import create_engine, text
 from extra_streamlit_components import CookieManager
-from fpdf import FPDFgit
+from fpdf import FPDF
 
 # --- ១. ការរៀបចំការភ្ជាប់ទៅ Supabase ---
 # ត្រូវប្រាកដថាបានដាក់ [database] url ក្នុង Streamlit Secrets រួចរាល់
